a979b5110ddf8209ae34b3269cec4e9cc4d6ba8d
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2022-08-08 13:46:25 +0100

    intial edit with JS

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('tomatodiv').style.background ="black"
+
+var seconddiv = document.getElementById('tomatodiv')
+
+seconddiv.addEventListener ('click', function (){
+    alert ('tomato div is clicked')
+})
